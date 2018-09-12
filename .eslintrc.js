75a6a5c13600f139d2df4cb738f98f94ca9bdee7
Tomáš Vojtášek <tomas.vojtasek@quantlane.com>
2018-09-12 09:46:55 +0200

    Eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,5 @@ module.exports = {
     node: true
   },
   plugins: ["prettier", "react"],
-  rules: {
-    "prettier/prettier": "error",
-    "prefer-const": "error",
-    "no-use-before-define": "error",
-    "no-var": "error",
-    "no-throw-literal": "error",
-    // Light console usage is useful but remove debug logs before merging to master.
-    "no-console": "off"
-  },
   extends: ["prettier", "plugin:react/recommended", "eslint:recommended"]
 };
