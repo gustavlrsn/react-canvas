815b3a78679d89591d697408a5de34cca866af37
Antonín Kříž <15214494+antoninkriz@users.noreply.github.com>
2023-01-06 16:56:22 +0100

    Using LRU Cache instead of the old backingStore

--- src/LRUCache.js
+++ src/LRUCache.js
@@ -9,138 +9,139 @@ function LRULinkListEntry(hash, obj) {
   this._next = null
 }
 
-// My favorite so far.
-function LRUCacheByLinkList(length) {
-  this.maxLength = length
-  this.reset()
-}
-
-LRUCacheByLinkList.prototype.reset = () => {
-  this.length = 0
-  this._top = null
-  this._bottom = null
-  this._keyVals = {}
-}
-
-LRUCacheByLinkList.prototype.detachElement = entry => {
-  const prev = entry._prev
-  const next = entry._next
-
-  if (prev !== null) {
-    prev._next = next
+class LRUCacheByLinkList {
+  constructor(length) {
+    this.maxLength = length
+    this.reset()
   }
 
-  if (next !== null) {
-    next._prev = prev
+  reset = () => {
+    this.length = 0
+    this._top = null
+    this._bottom = null
+    this._keyVals = {}
   }
 
-  if (entry === this._top) {
-    this._top = next
+  detachElement = entry => {
+    const prev = entry._prev
+    const next = entry._next
+
+    if (prev !== null) {
+      prev._next = next
+    }
+
+    if (next !== null) {
+      next._prev = prev
+    }
+
+    if (entry === this._top) {
+      this._top = next
+    }
+
+    if (entry === this._bottom) {
+      this._bottom = prev
+    }
+
+    this.length--
+    return entry
   }
 
-  if (entry === this._bottom) {
-    this._bottom = prev
+  removeElement = entry => {
+    this.detachElement(entry)
+    delete this._keyVals[entry._hash]
+    return entry._datum
   }
 
-  this.length--
-  return entry
-}
+  insertAtTop = entry => {
+    if (this._top !== null) {
+      this._top._prev = entry
+      entry._next = this._top
+    }
 
-LRUCacheByLinkList.prototype.removeElement = entry => {
-  this.detachElement(entry)
-  delete this._keyVals[entry._hash]
-  return entry._datum
-}
+    if (this._bottom === null) {
+      this._bottom = entry
+    }
 
-LRUCacheByLinkList.prototype.insertAtTop = entry => {
-  if (this._top !== null) {
-    this._top._prev = entry
-    entry._next = this._top
+    this._top = entry
+    this.length++
   }
 
-  if (this._bottom === null) {
-    this._bottom = entry
+  insertAtBottom = value => {
+    if (this._bottom !== null) {
+      this._bottom._next = value
+      value._prev = this._bottom
+    }
+
+    if (this._top === null) {
+      this._top = value
+    }
+
+    this._bottom = value
+    this.length++
   }
 
-  this._top = entry
-  this.length++
-}
+  set = (key, value) => {
+    const existinEl = this._keyVals[key]
 
-LRUCacheByLinkList.prototype.insertAtBottom = value => {
-  if (this._bottom !== null) {
-    this._bottom._next = value
-    value._prev = this._bottom
+    // Handling for dups
+    if (existinEl) {
+      if (existinEl._datum === value) {
+        // Don't change anything
+        return
+      }
+
+      // hardest part of code.
+      this.removeElement(existinEl)
+    }
+
+    value = new LRULinkListEntry(key, value)
+    this._keyVals[key] = value
+
+    // Most likely it will only be equal
+    // to purge the least used
+
+    if (this.length === this.maxLength) {
+      this.removeLeastUsed()
+    }
+
+    this.insertAtTop(value)
   }
 
-  if (this._top === null) {
-    this._top = value
+  removeLeastUsed = () => {
+    // Buhahah this is easy.
+    return this.removeElement(this._bottom)
   }
 
-  this._bottom = value
-  this.length++
-}
+  get = key => {
+    const value = this._keyVals[key]
 
-LRUCacheByLinkList.prototype.set = (key, value) => {
-  const existinEl = this._keyVals[key]
+    if (value !== undefined) {
+      // Promote this as it got used
+      this.promote(value)
+      return value._datum
+    }
 
-  // Handling for dups
-  if (existinEl) {
-    if (existinEl._datum === value) {
-      // Don't change anything
+    return null
+  }
+
+  // Remove the element
+  // and push it from the front
+  // so least recently used objects will end up at the end.
+  promote = el => {
+    // No need to promote
+    if (el === this.top) {
       return
     }
 
-    // hardest part of code.
-    this.removeElement(existinEl)
+    // seriously do we need to remove it ?
+    this.detachElement(el)
+    this.insertAtTop(el)
   }
 
-  value = new LRULinkListEntry(key, value)
-  this._keyVals[key] = value
-
-  // Most likely it will only be equal
-  // to purge the least used
-
-  if (this.length === this.maxLength) {
-    this.removeLeastUsed()
+  // Call this method
+  forEach = cb => {
+    Object.keys(this._keyVals).forEach(entry => cb(entry._datum))
   }
-
-  this.insertAtTop(value)
-}
-
-LRUCacheByLinkList.prototype.removeLeastUsed = () => {
-  // Buhahah this is easy.
-  return this.removeElement(this._bottom)
-}
-
-LRUCacheByLinkList.prototype.get = key => {
-  const value = this._keyVals[key]
-
-  if (value !== undefined) {
-    // Promote this as it got used
-    this.promote(value)
-    return value._datum
-  }
-
-  return null
-}
-
-// Remove the element
-// and push it from the front
-// so least recently used objects will end up at the end.
-LRUCacheByLinkList.prototype.promote = el => {
-  // No need to promote
-  if (el === this.top) {
-    return
-  }
-
-  // seriously do we need to remove it ?
-  this.detachElement(el)
-  this.insertAtTop(el)
-}
-
-// Call this method
-LRUCacheByLinkList.prototype.forEach = cb => {
-  Object.keys(this._keyVals).forEach(entry => cb(entry._datum))
 }
 
 module.exports = LRUCacheByLinkList
